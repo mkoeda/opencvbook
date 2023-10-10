10fd916817de5906b93a391d2dfc002f53087f6f
Masanao KOEDA <mkoeda@gmail.com>
2023-10-10 12:56:08 +0900

    update 10-4-2-2.py

--- OpenCVによる画像処理入門/第3版/第2刷/sampleprogram/10-4-2-2.py
+++ OpenCVによる画像処理入門/第3版/第2刷/sampleprogram/10-4-2-2.py
@@ -10,11 +10,18 @@ img_src = cv2.imread(file_src, cv2.IMREAD_COLOR)
 cv2.namedWindow('src')
 cv2.namedWindow('dst')
 
-center = tuple(np.array([img_src.shape[1] * 0.5, img_src.shape[0] * 0.5]))
+# 回転： 45[deg],  スケーリング： 1.0[ 倍 ]
 angle = 45.0
 scale = 1.0
-size = tuple(np.array([img_src.shape[1], img_src.shape[0]]))
+
+# 回転中心：画像の中心
+center = tuple(np.array([img_src.shape[1] * 0.5, img_src.shape[0] * 0.5]))
+
+# 以上の条件から 2 次元の回転行列を計算
 rot_mat = cv2.getRotationMatrix2D(center, angle, scale)
+
+# アフィン変換を行う
+size = tuple(np.array([img_src.shape[1], img_src.shape[0]]))
 img_dst = cv2.warpAffine(img_src, rot_mat, size, flags = cv2.INTER_CUBIC)
 
 cv2.imshow('src', img_src)  # 入力画像を表示
