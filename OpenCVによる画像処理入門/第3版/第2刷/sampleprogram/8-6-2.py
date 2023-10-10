20eabc1f8721ba2bc6f3b19b8bc8d17a0b20a57c
Masanao KOEDA <mkoeda@gmail.com>
2023-10-10 12:37:05 +0900

    Update 8-6-2.py

--- OpenCVによる画像処理入門/第3版/第2刷/sampleprogram/8-6-2.py
+++ OpenCVによる画像処理入門/第3版/第2刷/sampleprogram/8-6-2.py
@@ -10,8 +10,8 @@ img_src = cv2.imread(file_src, cv2.IMREAD_GRAYSCALE)
 cv2.namedWindow('src')
 cv2.namedWindow('dst')
 
-num_lab, img_lab = cv2.connectedComponents(img_src)
-img_dst = cv2.compare(img_lab, 1, cv2.CMP_EQ)
+num_lab, img_lab = cv2.connectedComponents(img_src)  # ラベリング
+img_dst = cv2.compare(img_lab, 1, cv2.CMP_EQ)  # ラベル1を抜き出し
 
 cv2.imshow('src', img_src)
 cv2.imshow('dst', img_dst)
